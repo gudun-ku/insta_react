2b6cd5fe25552b4124bcd815dabecdc1fe800023
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2019-10-24 08:39:27 +0400

    Adds post rendering

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import InstaService from '../services/instaService';
+import User from './User';
 
 export default class Posts extends Component {
   InstaService = new InstaService();
@@ -38,10 +39,35 @@ export default class Posts extends Component {
     this.updatePosts();
   }
 
+  renderItems(arr) {
+    return arr.map(item => {
+      const {name, altname, photo, src, alt, descr, id} = item;
+      return (
+        <div key={id} className="post">  
+        <User 
+          src={photo}
+          alt={altname}
+          name={name}
+          min={true}
+        />      
+        <img src={src} alt={alt}></img>
+        <div className="post__name">
+          {name}
+        </div>
+        <div className="post__descr">
+         {descr}
+        </div>
+      </div>
+      );
+    });
+  }
+
   render() {
+    const {error, posts} = this.state;
+    const items = this.renderItems(posts);
     return (
       <div className="left">
-        
+        {items}
       </div>
     )
   }
